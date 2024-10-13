f45cac2bf2c5cdbce49f4ae5e1a8c0cf0c8124b5
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-10-13 12:40:08 +0100

    set up browser router package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
+import './App.css';
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello</h1>
+      <h1 className="text-3xl font-bold text-center">
+        Hello
+      </h1>
     </>
   )
 }
